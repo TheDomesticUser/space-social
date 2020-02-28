2b58e7b9bc35be374452866c645f977ddc2aee44
TheDomesticUser <TheDomesticUser@gmail.com>
2020-02-28 16:00:50 -0500

    Added user signup and password hashing

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from . import models
+
+# Signup and login functionality
+from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.hashers import make_password
+
+class UserSignUp(CreateView):
+    model = models.User
+
+    fields = ['username', 'password']
+
+    def post(self, request):
+        # hash the password
+        password = make_password(request.instance.password)
+
+        request.instance.password = password
+
+        return super(self, UserSignUp).post(self.request)
+
+class UserLogin(LoginView):
+    pass
