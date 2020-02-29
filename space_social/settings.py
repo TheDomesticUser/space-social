c10fb994d1514d68c4d8f70d99c6b954b1545cb0
TheDomesticUser <TheDomesticUser@gmail.com>
2020-02-29 10:56:53 -0500

    Switched from sqlite3 to mysql

--- space_social/settings.py
+++ space_social/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'space_social.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'USER': 'dbadmin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
