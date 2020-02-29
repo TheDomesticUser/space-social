83a601bd05002e95e7f8c43e13e1943957f18c1e
TheDomesticUser <TheDomesticUser@gmail.com>
2020-02-29 10:08:39 -0500

    Datetime fields now automatically default to the current time and usernames now have to be unique

--- basic_app/models.py
+++ basic_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=20)
+    username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=20)
     
     def __str__(self):
@@ -10,7 +10,7 @@ class User(models.Model):
 class Group(models.Model):
     name = models.CharField(max_length=64)
     members_count = models.PositiveIntegerField()
-    date_created = models.DateField()
+    date_created = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -18,7 +18,7 @@ class Group(models.Model):
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=1024)
-    datetime_posted = models.DateTimeField()
+    datetime_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.author.username + ': ' + self.content
