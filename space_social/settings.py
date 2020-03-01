4c10c18ae9fffb37ba841f3a246727a0dbfacf9f
TheDomesticUser <TheDomesticUser@gmail.com>
2020-02-29 19:26:22 -0500

    Changed the database back to sqlite3 due to hashed password max length issues in MySql

--- space_social/settings.py
+++ space_social/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'space_social.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangodb',
-        'USER': 'dbadmin',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': 3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
