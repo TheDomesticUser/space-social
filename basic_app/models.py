69d6d92be461ab7a60d0e2207d89036ad9bd1df0
TheDomesticUser <TheDomesticUser@gmail.com>
2020-03-03 18:24:19 -0500

    Group leader is now defined in group model, instead of assocation, because it would take a lot less memory, as there would be only one leader in the group, and the rest would have is_leader=False

--- basic_app/models.py
+++ basic_app/models.py
@@ -14,6 +14,7 @@ class User(AbstractBaseUser):
         return self.username
 
 class Group(models.Model):
+    leader = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=32, unique=True)
     members_count = models.PositiveIntegerField(default=1)
     date_created = models.DateField(auto_now_add=True)
@@ -32,7 +33,6 @@ class Post(models.Model):
 class Association(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     member = models.ForeignKey(User, on_delete=models.CASCADE)
-    is_leader = models.BooleanField(default=False)
 
     def __str__(self):
         return self.group.name + ': ' + self.member.username
